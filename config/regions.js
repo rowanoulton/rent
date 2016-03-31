f64fbbd2303499012ede0fdf42b6133f0bfef32d
Rowan Oulton <rowan.oulton@gmail.com>
2016-03-31 13:00:15 +1300

    Disable cities other than AKL for testing

--- config/regions.js
+++ config/regions.js
@@ -1,16 +1,20 @@
 'use strict'
 
+const auckland = {
+  name: 'auckland',
+  id: 1
+}
+
+const wellington = {
+  name: 'wellington',
+  id: 15
+}
+
+const waikato = {
+  name: 'waikato',
+  id: 14
+}
+
 module.exports = [
-  {
-    name: 'auckland',
-    id: 1
-  },
-  {
-    name: 'wellington',
-    id: 15
-  },
-  {
-    name: 'waikato',
-    id: 14
-  }
+  auckland
 ]
